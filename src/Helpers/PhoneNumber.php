e7f1baabfba2b873efa782e81c53afc1f7a84b77
dena <dena.ajdari@gmail.com>
2018-08-08 14:14:24 +0430

    Check Mobile Phones updated. Farsi language files added.

--- src/Helpers/PhoneNumber.php
+++ src/Helpers/PhoneNumber.php
@@ -6,39 +6,18 @@ class PhoneNumber
 {
     public static function IranMobileValidation($phone)
     {
-		$mobile_codes =	[
-			'910',	'911',	'912',	'913',	'914',	'915',	'916',	'916',	'918',
-			'919',	'990',	'930',	'933',	'935',	'936',	'936',	'938',	'939',
-			'901',	'902',	'903',	'920',	'921',	'931',	'932',	'934',
-		];
-
 		$phone	= trim($phone);
 		$non_digits = ['‌', '-', '(', ')'];
 		$phone	= str_replace($non_digits, '', $phone);
+
 		$phone	= preg_replace('!\s+!', '', $phone);
-
-		if (substr($phone, 0, 6) == '009809') {
-			$phone	= '9'.substr($phone, 6);
-		} elseif (substr($phone, 0, 5) == '00989') {
-			$phone	= '9'.substr($phone, 5);
-		} elseif (substr($phone, 0, 5) == '+9809') {
-			$phone	= '9'.substr($phone, 5);
-		} elseif (substr($phone, 0, 4) == '+989') {
-			$phone	= '9'.substr($phone, 4);
-		} elseif (substr($phone, 0, 4) == '9809') {
-			$phone	= '9'.substr($phone, 4);
-		} elseif (substr($phone, 0, 3) == '989') {
-			$phone	= '9'.substr($phone, 3);
-		} elseif (substr($phone, 0, 2) == '09') {
-			$phone	= '9'.substr($phone, 2);
-		} else {
+		if (!preg_match('/^(\+989|00989|989|09|9)([0-9]{9})$/', $phone, $phone)) {
 			return false;
 		}
+		$phone = $phone[0];
 
-		if (!in_array(substr($phone, 0, 3), $mobile_codes)) {
-			return false;
-		}
-
-		return "+98$phone";
+		$phone	= preg_replace('/^(\+989|00989|989|09|9)/', '+989', $phone);
+		
+		return $phone;
 	}
 }
